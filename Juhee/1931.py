af4926ebe9ea612a5b567630de0877071ddbb432
JH Park <54497150+juliajh@users.noreply.github.com>
2021-09-14 02:27:46 +0900

    Update 1931.py

--- Juhee/1931.py
+++ Juhee/1931.py
@@ -1,5 +1,3 @@
-from itertools import combinations
-
 num = int(input())
 conference_list=[[0 for col in range(2)] for row in range(num)]
 num_list=list(range(num))
@@ -9,6 +7,18 @@ for i in range (num):
     conference_list[i][0]=start
     conference_list[i][1]=end
 
-for i in range (1,num+1):
-    print(list(combinations(num_list,i)))
+conference_list=sorted(conference_list,key=lambda x:x[0])
+conference_list=sorted(conference_list,key=lambda x:x[1]) #끝나는 시간이 빠른 순서
+
+maxnum=0
+start=0
+for conference in conference_list:
+    if conference[0]>=start:
+        maxnum+=1
+        start=conference[1]
+
+print(maxnum)
+
+
+
 
