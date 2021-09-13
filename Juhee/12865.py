b82d6d75a7d776ec11cf39a6343868518880d06d
JH Park <54497150+juliajh@users.noreply.github.com>
2021-09-14 02:34:07 +0900

    박주희 1주차

--- Juhee/12865.py
+++ Juhee/12865.py
@@ -1,4 +1,5 @@
-from itertools import combinations
+#최종적으로 몇개의 짐을 가져갈지 모른다는 점에서 많은 고민을 한 문제.
+from itertools import combinations  #조합을 이용해 볼 수 있는 패키지 
 
 N,K=map(int,input().split())
 
@@ -11,14 +12,16 @@ for i in range (N):
     dic[W]=V
 
 for i in range(1,N+1):
-    cb.append(list(combinations(list(dic.keys()),i)))
+    cb.append(list(combinations(list(dic.keys()),i))) #가능한 모든 조합(1개~N개)
 
-for i in range (len(cb)):
+#가져가는 개수를 기준으로 loop를 돌려봄
+#불필요한 시간과 메모리를 잡아먹는 것이 아닌지..?
+for i in range (len(cb)):    
     if sum_num!=0:
         sum_list.append(sum_num)
     sum_num=0
     for j in range (len(cb[i])):
-        a_list=list(cb[i][j])   #조합 결정
+        a_list=list(cb[i][j])  
         if (sum(a_list)<=N):
             for k in range (len(a_list)):
                 sum_num+=dic.get(a_list[k])
