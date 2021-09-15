791e737a331097bfc4c9d3489b0c554a6d20a43c
Bae-jihyun <govl6113@naver.com>
2021-09-15 23:12:10 +0900

    피드백 수정

--- Bae-jihyun/1520.py
+++ Bae-jihyun/1520.py
@@ -2,7 +2,7 @@ import sys
 
 direction=[(0,-1),(0,1),(-1,0),(1,0)]  #방향성
 
-def calc(result, lst, m,n,x,y):
+def calc(x,y):
     if x ==m-1 and y ==n-1:             #오른쪽 맨아래(마지막지점) 근처 좌표라면
         result[x][y]=1                  #마지막 지점으로 가는 길은 1임.
         return
@@ -14,10 +14,9 @@ def calc(result, lst, m,n,x,y):
         ny=y+dy                         #다음 y좌표구하기
         if 0<=nx<m and 0<=ny<n:         #다음 x,y좌표가 유효한지(m,n보다 큰 인덱스인지)
             if(lst[x][y]> lst[nx][ny]):  #현재 좌표와 다음 좌표 값비교 (현재보다 낮은 길일때만)
-               calc(result, lst, m,n,nx,ny)   #다음 좌표도 똑같이 계산
+               calc(nx,ny)   #다음 좌표도 똑같이 계산
                result[x][y] +=result[nx][ny]  #갈 수 있는 방향의 점이 가진 값(마지막지점으로 갈 수 있는 길 갯수)을 더해줌.
                
-
 
 m, n = map(int, input().split())
 
@@ -28,7 +27,7 @@ for i in range(m):                 #데이터 받기
 
 
 result = [[-1]*n for i in range(m)] #빈 배열 만들기(아직 방문하지 않았다는 뜻: -1)
-calc(result, lst, m,n,0,0)          #(0,0)에 대한 계산
+calc(0,0)          #(0,0)에 대한 계산
 print(result[0][0])
 
 
@@ -37,6 +36,7 @@ print(result[0][0])
 #마지막 지점근처부터 마지막지점으로 갈 수 있는 길의 수를 구한다.
 #한 점의 4개의 방향의 숫자를 다 더해준다.
 #첫번째 좌표 오른쪽과 아래 인덱스의 값을 더해준다. 
+
 
 """
 피드백 코멘트 :
