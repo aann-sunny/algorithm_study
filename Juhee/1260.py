05047327493444933678707680c3213db593c1ad
juhee <juliajuhee222@gmail.com>
2021-09-21 00:54:01 +0900

    박주희 week2

---
+++ Juhee/1260.py
@@ -0,0 +1,36 @@
+import collections
+import sys
+
+#재귀를 이용하여 가장 깊이 이동
+def dfs(V):
+    print(V,end=' ')
+    visited[V]=True  #첫 노드 방문
+    for i in range(1,N+1):
+        if visited[i]==False and graph[V][i]==1:  #방문X, 노드 연결되어있다면
+            dfs(i)  
+
+#큐 이용          
+def bfs(V):
+    queue=[V]
+    visited[V]=True
+    while(queue):
+        V=queue.pop(0) #가장 밑에 있는걸(가장 먼저 들어온 것) 빼고 V에 저장
+        print(V,end=' ')
+        for i in range(1,N+1):
+            if visited[i]==False and graph[V][i]==1: #방문X, 노드 연결되어있다면
+                queue.append(i)
+                visited[i]=True
+
+N,M,V = map(int,sys.stdin.readline().split())
+graph=[[0]*(N+1) for i in range(N+1)]
+visited=[False for i in range(N+1)]  #방문 여부
+
+for i in range(M):
+    a,b = map(int,sys.stdin.readline().split())
+    graph[a][b]=1
+    graph[b][a]=1  #양뱡향이므로
+
+dfs(V)
+print()
+visited=[False for i in range(N+1)]
+bfs(V)
---
+++ Juhee/1920.py
@@ -0,0 +1,15 @@
+import sys
+
+N=int(sys.stdin.readline())
+A=set(map(int,sys.stdin.readline().split()))  #list 사용할 시 시간 초과 --> set
+#list vs set --> list: 순서 O set: 순서 X
+#set이 list보다 시간 복잡도 면에서 효율적
+
+M=int(sys.stdin.readline())
+B=list(map(int,sys.stdin.readline().split()))
+
+for i in B:
+    if i in A:
+        print(1)
+    else:
+        print(0)
