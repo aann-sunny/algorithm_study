6e22b3d39be546957c2636185b33a3afd62c742b
juhee <juliajuhee222@gmail.com>
2021-10-12 00:55:55 +0900

    week 4-1

---
+++ Juhee/4주차/2166.py
@@ -0,0 +1,15 @@
+import math
+N  = int(input())
+ass = [[]]*N
+for i in range(N):
+    ass[i] = list(map(int, input().split()))
+    
+ass.append(ass[0])
+left=0
+right =0
+
+for i in range(N):
+    left+=ass[i][0]*ass[i+1][1]
+    right +=ass[i][1]*ass[i+1][0]
+
+print(((round(abs(left-right)/2*100)//10)/10))
