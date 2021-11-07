f15ca6310bddfcb40a4bc9490ae3af1ab8795a72
Bae-jihyun <govl6113@naver.com>
2021-11-07 14:33:36 +0900

    flake8 적용

--- Bae-jihyun/4주차/2533.py
+++ Bae-jihyun/4주차/2533.py
@@ -13,7 +13,10 @@ def dfs(node):
             continue
         dfs(t)
         graph[node][0] += graph[t][1]
-        graph[node][1] += min(graph[t][0], graph[t][1])  # 현재 노드가 얼리 어답터인 경우, 연결된 친구들은 얼리 어답터이거나 아니어도 됨. 즉, 얼리 어답터인 친구(자식)와 아닌 친구(자식) 중 최소인 경우를 더해줍니다.
+
+        # 현재 노드가 얼리 어답터인 경우, 연결된 친구들은 얼리 어답터이거나 아니어도 됨.
+        # 즉, 얼리 어답터인 친구(자식)와 아닌 친구(자식) 중 최소인 경우를 더해줌.
+        graph[node][1] += min(graph[t][0], graph[t][1])
         print(node, t, graph)
 
 
@@ -40,8 +43,11 @@ print(min(graph[1][0], graph[1][1]))
 '''
 서브트리로 계속 내려가야하기 때문에 bfs보다는 dfs사용
 
-graph[node][0] += graph[friend][1];             //현재 노드가 얼리가 아닌 경우, 연결된 친구들은 모두 얼리 어답터여야 함. 즉, 연결된 친구(자식)들이 얼리인 경우를 더해줌.
-graph[node][1] += Math.min(graph[friend][0], graph[friend][1]);  //현재 노드가 얼리인 경우, 얼리인 자식과 아닌 자식 중 최소를 더함
+현재 노드가 얼리가 아닌 경우, 연결된 친구들은 모두 얼리 어답터여야 함. 즉, 연결된 친구(자식)들이 얼리인 경우를 더해줌.
+graph[node][0] += graph[friend][1];
+
+현재 노드가 얼리인 경우, 얼리인 자식과 아닌 자식 중 최소를 더함.
+graph[node][1] += Math.min(graph[friend][0], graph[friend][1]);
 
 주석해놓은 print의 주석을 풀면 계산과정 쉽게 확인가능
 '''
