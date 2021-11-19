442449f16e167212dd7e8a2b122ca48ff8da5076
Bae-jihyun <govl6113@naver.com>
2021-11-19 15:57:57 +0900

    8주차 12865 주석추가

--- Bae-jihyun/8주차/12865.py
+++ Bae-jihyun/8주차/12865.py
@@ -1,6 +1,6 @@
 n, k = map(int, input().split())  # n:물품의 수, k:버틸 수 있는 무게
-lst = [[0, 0]]
-result = [[0] * (k+1) for _ in range(n+1)]
+lst = [[0, 0]]                    # lst: 입력받는 배열
+result = [[0] * (k+1) for _ in range(n+1)]  # result: dp에 사용되는 배열
 
 for _ in range(n):
     lst.append(list(map(int, input().split())))
@@ -13,7 +13,7 @@ for i in range(1, n+1):     # 가방에 넣어지는 물건
         if j >= w:
             result[i][j] = max(result[i-1][j-w] + v, result[i-1][j])  # 아래 참고
         else:
-            # j가 m보다 작으면 i-1번째의 j를 가져온다.
+            # j가 w보다 작으면 i-1번째의 j를 가져온다.
             result[i][j] = result[i-1][j]
 
 
@@ -25,8 +25,8 @@ result[i][j] = max(lst[i][j-w] + v, lst[i-1][1])
 lst[i-1][j-w]+v: 현재 물건의 가치(v)와 현재 물건을 넣고 나면 j무게에서 남는 나머지 무게의 최대
 lst[i-1][1]: j무게에서 여태까지 중 제일 높은 가치
 
-만약 j가 6이면 6키로를 넣을 자리가 있는데 5키로인 물건을 가방에 넣고 싶다면, 1키로의 여유분이 남는다.
-그럼 1키로일때 최대 가치인 lst[i-1][1](lst[i-1][j-w])와 현재 물건의 가치를 더한다.(5키로와 1키로의 최대가치)
+만약 j가 6이면 6키로를 넣을 자리가 있는데 w가 5키로인 물건을 가방에 넣고 싶다면, 1키로의 여유분이 남는다.
+그럼 1키로일때 최대 가치인 lst[i-1][1](lst[i-1][j-w])와 현재 물건의 가치(v)를 더한다.(5키로와 1키로의 최대가치)
 또는 i-1일때 j의 최대 가치(현재 물건을 추가하기 전)의 max를 구해 result[i][j]에 넣는다.
 
 
